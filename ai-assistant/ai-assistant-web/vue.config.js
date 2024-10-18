30091518e1081071b21597fd2624967828e7165a
yang <yang@qq.com>
2024-10-18 16:19:23 +0800

    wip

--- ai-assistant/ai-assistant-web/vue.config.js
+++ ai-assistant/ai-assistant-web/vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    // host: 'localhost', 
+    port: 443,
+    // historyApiFallback: true,
     allowedHosts: "all"
   }
 })
