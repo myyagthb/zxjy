4224f5f2bbda4fe447c7fc9432dcc1b88d634b81
yang <yang@qq.com>
2024-11-05 16:14:33 +0800

    add teacher page

---
+++ ai-assistant/online-education-web/src/views/teacher/TeacherAside.vue
@@ -0,0 +1,56 @@
+
+
+<template>
+  <div>
+    <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        v-model:selectedKeys="selectedKeys"
+        :openKeys="['student']"
+        @open="handleOpen"
+        @close="handleClose"
+    >
+      <el-menu-item index="1" key="/my-course">
+        <router-link to="/teacher/my-course" style="text-decoration: none">
+          <span>我的课程</span>
+        </router-link>
+      </el-menu-item>
+      <el-menu-item index="3" key="/upload">
+        <router-link to="/teacher/upload" style="text-decoration: none">
+          <span>上传课程</span>
+        </router-link>
+      </el-menu-item>
+      <el-menu-item index="4" key="/kcrl">
+        <router-link to="/kcrl" style="text-decoration: none">
+          <span>课程日历</span>
+        </router-link>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script setup>
+
+import router from "@/router";
+import {ref, watch} from "vue";
+
+const handleOpen = (key, keyPath) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key, keyPath) => {
+  console.log(key, keyPath)
+}
+
+
+const selectedKeys = ref([]);
+watch(() => router.currentRoute.value.path, (newValue) => {
+  console.log('watch', newValue);
+  selectedKeys.value = [];
+  selectedKeys.value.push(newValue);
+}, {immediate: true});
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ ai-assistant/online-education-web/src/views/teacher/uploadCourse.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="my_course">
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="全部" name="first">
+
+        <div class="course_list">
+          <div class="course_item" v-for="(item,index) in itemList" :key="index" >
+            <div class="course_item_img_div">
+              <img :src="item.courseImg" >
+            </div>
+            <div class="item_title">
+              {{item.courseTitle}}
+            </div>
+            <div class="item_progress">
+              <el-progress :percentage="item.percentage" />
+            </div>
+            <div class="item_desc">
+              已学习{{item.learned}}/{{item.total}}节
+            </div>
+          </div>
+        </div>
+
+        <div style="text-align: left;">
+          <h2>
+            &emsp;&emsp;&emsp;课程推荐
+          </h2>
+        </div>
+
+        <div class="course_list">
+          <div class="course_item" v-for="(item,index) in itemList" :key="index" >
+            <div class="course_item_img_div">
+              <img :src="item.courseImg" >
+            </div>
+            <div class="item_title">
+              {{item.courseTitle}}
+            </div>
+            <div class="item_progress">
+              <el-progress :percentage="item.percentage" />
+            </div>
+            <div class="item_desc">
+              已学习{{item.learned}}/{{item.total}}节
+            </div>
+          </div>
+        </div>
+
+      </el-tab-pane>
+
+    </el-tabs>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const activeName = ref('first')
+
+const handleClick = (tab, event) => {
+  console.log(tab, event)
+}
+
+
+const itemList = ref([
+  {
+    courseImg: require('@/assets/images/student/vue-img.jpeg'),  //课程封面
+    courseTitle: "vue课程",      //课程标题
+    percentage: 20,                       //已学习占比
+    customColor: "#409eff",               //进度条颜色
+    learned: 2,                           //已学习课程数
+    total: 8,                             //课程总数
+  },{
+    courseImg: require('@/assets/images/student/suanfa-img.jpg'),  //课程封面
+    courseTitle: "数据结构与算法",      //课程标题
+    percentage: 20,                       //已学习占比
+    customColor: "#409eff",               //进度条颜色
+    learned: 2,                           //已学习课程数
+    total: 8,                             //课程总数
+  },{
+    courseImg: require('@/assets/images/student/weixin-img.png'),  //课程封面
+    courseTitle: "微信小程序入门与实践",      //课程标题
+    percentage: 20,                       //已学习占比
+    customColor: "#409eff",               //进度条颜色
+    learned: 2,                           //已学习课程数
+    total: 8,                             //课程总数
+  },{
+    courseImg: require('@/assets/images/student/python-img.jpeg'),  //课程封面
+    courseTitle: "Python入门加进阶",      //课程标题
+    percentage: 20,                       //已学习占比
+    customColor: "#409eff",               //进度条颜色
+    learned: 2,                           //已学习课程数
+    total: 8,                             //课程总数
+  }
+])
+
+</script>
+
+<style scoped>
+.demo-tabs{
+  width: 100%;
+}
+
+.course_list{
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: self-start;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+
+.course_item{
+  width: 20%;
+  margin-left: 4%;
+}
+
+.course_item_img_div>img{
+  width: 100%;
+  height: 25%;
+  min-height: 160px;
+  border-radius: 10px;
+}
+
+.item_title{
+  margin-top: 5%;
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.item_progress{
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+
+.item_desc{
+  text-align: left;
+}
+</style>
