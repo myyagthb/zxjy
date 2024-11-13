ca1ee49316d3da384f19f32c65a122b374877475
zhoujinyu <2319109590@qq.com>
2024-11-13 10:03:22 +0800

    docker设置

--- ai-assistant/online-education-web/vue.config.js
+++ ai-assistant/online-education-web/vue.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer:{
+  devServer: {
     port: 443,
     allowedHosts: "all"
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 })
+
